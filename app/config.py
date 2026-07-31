1373a501ce7d8bb847ce7e761bf7ca282f38b199
AlexanderChoque <choquejulca.alexander88@gmail.com>
2026-07-31 15:16:08 -0500

    Implementacion_de_las_variables_de_entorno

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    app_name: str = "FastAPI App"
+    debug: bool = False
+    secret_key: str
+    database_url: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+settings = Settings()
